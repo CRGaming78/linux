44e473c68296b3bc06d94dda3840b076eacbbc92
CR <aryan.anupma5566@gmail.com>
2023-10-27 09:56:23 +0530

    first commit

---
+++ 2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "MENU"
+echo "1.B.tech CSE"
+echo "2.B.Sc CSE"
+echo "3.M.tech"
+echo "4.M.Sc"
+echo "Select any branch to know its subjects:"
+read a
+if [ $a="1" ]
+then
+echo "1.Maths 2.c programming 3. physics"
+else
+echo "I don't know"
+fi
+
