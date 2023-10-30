110fb389a4cdfa942bd7ab73a3005f64a20224fb
Aryan Kumar <81426042+CRGaming78@users.noreply.github.com>
2023-10-30 19:21:02 +0530

    Add files via upload
    Some checked programs remaing function.sh( work in progress)

---
+++ Basic Calculator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Enter 1st Number:"
+read num1
+echo "Enter 2nd Number:"
+read num2
+echo "MENU"
+echo "1.Add"
+echo "2.sub"
+echo "3.multi"
+echo "4.div"
+echo "What do you want to do:"
+read c
+case "$c" in
+#case 1
+"1" ) echo "Added number :" $((num1+num2)) ;;
+#case 2
+"2" ) echo "Sub Number :" $((num1-num2)) ;;
+#case 3
+"3" ) echo "Multi Number:" $((num1*num2)) ;;
+#case 4
+"4" ) echo "Div Number:" $((num1/num2)) ;;
+esac
---
+++ Function.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+function calc(){
+    var1=10
+    var2=30
+    var3=var1+var2
+    echo $var3
+}
