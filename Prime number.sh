8942fe5cc2c905e2c034749a4ce59b4fd5359cdc
Aryan Kumar <81426042+CRGaming78@users.noreply.github.com>
2023-11-02 23:57:34 +0530

    some more programs and edits

--- Prime number.sh
+++ Prime number.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 read -p "Enter a number: " num
+for ((i=2;i*i<=num;i++)); do 
+    if ["$((num%i))" -eq 0]; then
+        echo "It is not a prime number"
+    else
+        echo "It is a prime number"
+    fi
+done
