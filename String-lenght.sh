238346bee267440d4b6feea4637c12d07cd3fc47
Aryan Kumar <81426042+CRGaming78@users.noreply.github.com>
2023-12-01 10:09:17 +0530

    Minor edit

--- String-lenght.sh
+++ String-lenght.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-read -p "Enter a string" string
-string_length=$(expr length "$string")
-echo "The length of the string is: $string_length"
+read -p "Enter a string: " string
+length=$(expr length "$string")
+# you can also use length=${#string}
+echo "The length of the string is: $length"
