8fc7a039f44ea4d8b49f26d6267c8bead5456c77
Aryan Kumar <81426042+CRGaming78@users.noreply.github.com>
2023-12-17 09:02:58 +0530

    New programs

--- push.sh
+++ push.sh
@@ -1,3 +1,11 @@
-git add -A
-git commit -m "edit and new program"
+#/bin/bash
+read -p "Do you want to add all programs(all/0-9):" des
+if [[ $des == "all" ]]; then
+    git add -A
+#if [[ $des == "0-9" ]]; then
+else
+    echo "invaild input"
+fi
+read -p "Enter any msg: " msg
+git commit -m "$msg"
 git push
